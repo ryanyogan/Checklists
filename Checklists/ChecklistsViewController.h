efef11151f9b25a429e5bbbe9512edb6557675bc
Ryan Yogan <ryanyogan@gmail.com>
2014-06-06 20:36:11 -0500

    Initial Commit

---
+++ Checklists/ChecklistsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChecklistsViewController.h
+//  Checklists
+//
+//  Created by Ryan Yogan on 6/6/14.
+//  Copyright (c) 2014 Ryan Yogan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChecklistsViewController : UIViewController
+
+@end
---
+++ Checklists/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Checklists
+//
+//  Created by Ryan Yogan on 6/6/14.
+//  Copyright (c) 2014 Ryan Yogan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ChecklistsAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ChecklistsAppDelegate class]));
+    }
+}
