9ca01b5ce799b73875d56acc609d7896f0b166d3
Ryan Yogan <ryanyogan@gmail.com>
2014-06-09 13:40:13 -0500

    Updated

--- Checklists/ChecklistsViewController.m
+++ Checklists/ChecklistsViewController.m
@@ -26,4 +26,39 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 5;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ChecklistItem"];
+    
+    UILabel *label = (UILabel *)[cell viewWithTag:1000];
+    
+    if (indexPath.row == 0 ) {
+        label.text = @"Walk the dog";
+    } else if (indexPath.row == 1) {
+        label.text = @"Brush my teeth";
+    } else if (indexPath.row == 2) {
+        label.text = @"Learn iOS Development";
+    }
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    
+    if (cell.accessoryType == UITableViewCellAccessoryNone) {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    } else {
+        cell.accessoryType = UITableViewCellAccessoryNone;
+    }
+    
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
 @end
